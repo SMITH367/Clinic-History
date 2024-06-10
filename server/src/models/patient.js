cf659b74dcf5513c0e18f877f4e650e6c287a3bf
smith367 <brayansmithpuente@gmail.com>
2024-06-10 16:07:53 -0500

    fix patient search

--- server/src/models/patient.js
+++ server/src/models/patient.js
@@ -1,7 +1,7 @@
 const savePatient = "INSERT INTO patients SET ?";
 const getPatients = "SELECT patients.identification, patients.name, patients.last_name, patients.identificationType, classes.name as type FROM patients, classes WHERE patients.class_patient = classes.class_id";
 const getPatient = "SELECT patients.identification, patients.name, patients.last_name, patients.identificationType, classes.name as type FROM patients, classes WHERE patients.class_patient = classes.class_id and identification = ?"
-const searchPatient = "SELECT patients.identification, patients.name, patients.last_name, patients.identificationType, classes.name as type FROM patients, classes WHERE patients.class_patient = classes.class_id and patients.name LIKE ? OR patients.identification LIKE ?"
+const searchPatient = "SELECT patients.identification, patients.name, patients.last_name, patients.identificationType, classes.name as type FROM patients, classes WHERE patients.class_patient = classes.class_id AND (patients.name LIKE ? OR patients.identification LIKE ?)"
 const updatePatient = "UPDATE patients SET ? WHERE identification = ?"
 module.exports = {
     savePatient,
