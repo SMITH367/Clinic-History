65ab4dabf87b852b6aae20d3ed5f4f04b7f01f71
smith367 <brayansmithpuente@gmail.com>
2024-08-28 23:55:54 -0500

    fix: format pdf name in prescription

--- client/src/app/pages/prescription-print/prescription-print.component.ts
+++ client/src/app/pages/prescription-print/prescription-print.component.ts
@@ -22,12 +22,12 @@ export class PrescriptionPrintComponent {
     const doc = new jsPDF('p', 'cm', 'a4');
     doc.setFont('Arial');
 
-    doc.text('Receta Médica', 2, 4);
+    doc.text('RECETA MÉDICA', 2, 4);
     doc.text(`Paciente: ${this.prescriptionData.identification} ${this.prescriptionData.name} ${this.prescriptionData.last_name}`, 2, 5);
     doc.text(`Edad: ${calculateAge(this.prescriptionData.birthday)} años`, 2, 6);
     doc.text(`Fecha: ${formatDate(this.prescriptionData.date_prescription)}`, 2, 7);
     doc.text('Medicamentos:', 2, 8);
     doc.text(`${this.prescriptionData.prescription}`,2,9)
-    doc.save('receta-medica.pdf');
+    doc.save(`Receta-medica-${this.prescriptionData.name}-${this.prescriptionData.last_name}-${formatDate(this.prescriptionData.date_prescription)}.pdf`);
   }
 }
