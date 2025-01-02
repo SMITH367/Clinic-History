6183eaf28b9df483876b14be07ed3494101baa89
smith367 <brayansmithpuente@gmail.com>
2025-01-01 21:53:58 -0500

    local database added

--- server/src/database.js
+++ server/src/database.js
@@ -1,11 +1,10 @@
-const mysql = require('mysql2')
+const mysql = require('mysql')
 
 const conexion = mysql.createConnection({
-    host: "mysql-30cf7609-brayansmithpuente-1ae1.g.aivencloud.com",
-    user: "avnadmin",
-    port:23332,
-    password:"AVNS_AYmjwSWcNAEhyVsdBoK",
-    database:"defaultdb"
+    host: "localhost",
+    user: "root",
+    password:"",
+    database:"HistoryClinic"
 })
 conexion.connect((err) => {
     if (err) {
